8964b1f0d972e82063fd65404aed1319b8c71db0
Corey Frang <gnarf@gnarf.net>
2012-12-06 19:41:58 -0600

    bunch of random stuff for create dialog

--- client/client.js
+++ client/client.js
@@ -11,16 +11,27 @@ Template.auction_page.auction = function() {
   var auction = Auctions.findOne({ _id: Session.get( "auction-id" ) });
   console.log( auction );
   return auction;
-}
+};
 
 Template.auction_page.show = function() {
   return Session.get( "auction-id" );
-}
+};
 
 window.onhashchange = function() {
   Session.set( "auction-page", location.hash.match(/auctions/) );
   var idMatch = location.hash.match(/auction-([\w-]+)/);
   Session.set( "auction-id", idMatch && idMatch[1] );
+  jQuery(".nav a").each(function(){
+    var elem = jQuery(this);
+    elem.closest("li").toggleClass("active", this.hash == location.hash);
+  });
+};
+
+Template.page.rendered = function() {
+  if (!this.onlyonce) {
+    this.onlyonce = true;
+    window.onhashchange();
+  }
 };
 
 
@@ -30,49 +41,56 @@ window.onhashchange = function() {
 var openCreateDialog = function () {
   Session.set("createError", null);
   Session.set("showCreateDialog", true);
+  jQuery(Template.create_dialog.find(".modal")).removeClass("disabled");
 };
 
 Template.page.events({
-	'click input.create': function () {
-		openCreateDialog();
-	}
+  'click .create': function() {
+    openCreateDialog();
+  }
 });
 
-Template.page.showCreateDialog = function () {
+Template.create_dialog.show = function () {
   return Session.get("showCreateDialog");
 };
 
-Template.createDialog.events({
+Template.create_dialog.events({
   'click .save': function (event, template) {
-		// Pull out our field values
+    // Pull out our field values
+    if ( template.find(".modal.disabled") ) {
+      return;
+    }
     var title = template.find(".title").value,
-				duration = template.find(".duration").value;
-		// Ensure the Title Length
+        duration = template.find(".duration").value;
+    // Ensure the Title Length
     if(!title.length) {
-	    Session.set("createError",
+      Session.set("createError",
                   "Your auction needs a title.");
-		}
-		// Ensure the Duration
-		if(duration < 15) {
-	    Session.set("createError",
-                  "The duration must be greater than 15 minutes.");			
-		}
-		if(duration > 60) {
-	    Session.set("createError",
-                  "The duration must be less than 60 minutes.");			
-		}
-		// Ensure we have no errors before save
-		if(!Session.get("createError")) {
+    }
+    // Ensure the Duration
+    if(duration < 15) {
+      Session.set("createError",
+                  "The duration must be greater than 15 minutes.");
+    }
+    if(duration > 60) {
+      Session.set("createError",
+                  "The duration must be less than 60 minutes.");
+    }
+    // Ensure we have no errors before save
+    if(!Session.get("createError")) {
+      jQuery(template.find(".modal")).addClass("disabled");
       Meteor.call('createAuction', {
         title: title,
-				duration: duration,
+        duration: duration
       }, function (error, auction) {
-        if (!error) {
-          Session.set("selected_auction_id", auction);
+        jQuery(template.find(".modal")).removeClass("disabled");
+        if (error) {
+          Session.set("createError", "There was an error creating this auction: " + error);
+        } else {
+          window.location = "#auction-" + auction;
+          Session.set("showCreateDialog", false);
         }
       });
-      Session.set("showCreateDialog", false);
-    } else {
     }
   },
 
@@ -81,6 +99,6 @@ Template.createDialog.events({
   }
 });
 
-Template.createDialog.error = function () {
+Template.create_dialog.error = function () {
   return Session.get("createError");
 };
