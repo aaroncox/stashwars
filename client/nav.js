77199b1cebf149d97a6ce9a6c84c6c5187919726
CreepGin <st0724@gmail.com>
2012-12-08 01:48:11 -0500

    Redid nag link "active" toggling

--- client/nav.js
+++ client/nav.js
@@ -1,23 +1,23 @@
 Template.nav.nav_links = function() {
-	return [];
+	var links = [
+		{ name: "Auctions", href: "/auctions", active: "", templ: "auction_list" },
+		{ name: "Bids", href: "/bids", active: "", templ: "bids_page" },
+	];
+	for (var i in links){
+		var link = links[i];
+		if (Session.equals("currentpage", link.templ))
+			link.active = "active";
+	}
+	var auction = Template.auction_page.auction();
+	if (auction){
+		links.push({name: auction.title, href: "/auction/"+auction._id, active: "active" });
+	}
+	return links;
 }
 
 Template.nav.auction = function() {
 	Session.get("page");
 	return Template.auction_page.auction();
-};
-
-Template.nav.rendered = function() {
-	jQuery("a.local").each(function(){
-		var elem = jQuery(this),
-			linkPath = this.pathname.replace(/^\//,""),
-			locationPath = location.pathname.replace(/^\//,""),
-			locationHash = location.hash.replace(/^#/,"");
-		if ( !locationPath.length ) {
-			locationPath = locationHash;
-		}
-		elem.closest("li").toggleClass("active", elem.attr("data-templ") === Session.get("currentpage"));
-	});
 };
 
 Template.home.events({
