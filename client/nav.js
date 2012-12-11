0c172252614f4b236907afc1dbe3f38e80ced03b
CreepGin <st0724@gmail.com>
2012-12-11 01:37:35 -0500

    Improved Routing. You no longer need to specify a callback for a route. The "default" callback is automatically called if you don't explicitly specify one.
    Also fixed trailing slash issues. i.e. "auctions/" will default to "auctions"
    
    Added a Profile button to user account drop down menu. Also a Profile stub page.

--- client/nav.js
+++ client/nav.js
@@ -24,12 +24,21 @@ Template.home.events({
 	'click .create': openCreateDialog
 });
 
+//User Account Dropdown menu overrides and setups
 Template._loginButtonsLoggedInDropdownActions.rendered = function(){
+	//Move all this to a template if it gets too bloated here.
 	var $ = jQuery;
-	var ele = $('<div class="login-button">Create Auction</div>');
-	$("#login-buttons-open-change-password").before(ele);
-	ele.click(function(){
+	var createAuctionButton = $('<div id="create-auction" class="login-button">Create Auction</div>');
+	$("#login-buttons-open-change-password").before(createAuctionButton);
+	var userProfileButton = $('<div class="login-button" id="user-profile">My Profile</div>');
+	createAuctionButton.before(userProfileButton);
+}
+Template._loginButtonsLoggedInDropdownActions.events = {
+  'click #create-auction': function (event) {
 		openCreateDialog();
 		Accounts._loginButtonsSession.closeDropdown();
-	});
+	},
+	'click #user-profile': function (event) {
+		Meteor.Router.navigate("profile", {trigger: true});
+	}
 }
--- client/router.js
+++ client/router.js
@@ -1,27 +1,48 @@
 var StashWarsRouter = Backbone.Router.extend({
+
+	initialize: function() {
+		//Simplifying default page routing
+		for (var key in this.routes) {
+			var endpoint = this.routes[key];
+			if (!this[endpoint]) {
+				var router = this;
+				(function(){	//<-- this function block is needed to enforce proper scoping
+					var templateName = endpoint;
+					router[templateName] = function(){};
+					router.route(key, templateName, function () {
+						router.default(templateName);
+					});
+				})();
+			}
+		}
+		//Fixing trailing slashes
+		var re = new RegExp("(\/)+$", "g");
+		this.route(/(.*)\/+$/, "trailFix", function (id) {
+			id = id.replace(re, '');
+			this.navigate(id, true);
+		});
+	},
+
+	//Define custom routes below
 	routes: {
-		"": "index",
+		"": "home",
 		"auction/:auction_id": "auction",
-		"auctions": "auctionIndex",
-		"bids": "bidsPage"
+		"auctions": "auction_list",
+		"bids": "bids_page",
+		"profile": "user_profile",
+
+		"*action": "home"	//Everything not matched routes to Home right now; need to implememnt 404's later
 	},
-	index: function() {
-		Session.set("currentpage", "home");
-		Session.set("auction-id", false);
-	},
-	auctionIndex: function() {
-		Session.set("currentpage", "auction_list");
-		Session.set("auction-id", false);
+	default: function (pagename) {	//Used in initialize() for easy routing for page templates
+		Session.set("currentpage", pagename);
+		Session.set("auction-id", null );
 	},
 	auction: function( auction_id ) {
 		Session.set("currentpage", "auction_page");
 		Session.set("auction-id", auction_id );
 		Session.set("auction_page_bid_error", false);
-	},
-	bidsPage: function() {
-		Session.set("currentpage", "bids_page");
-		Session.set("auction-id", false);
 	}
+
 });
 
 Meteor.Router = new StashWarsRouter();
