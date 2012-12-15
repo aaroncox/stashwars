d847bcc2783622354625c46eeb569f49c93e1aac
Aaron Cox <aaron.cox@greymass.com>
2012-12-14 18:42:27 -0600

    Added the itemCount helper for handlebars.

--- client/helpers.js
+++ client/helpers.js
@@ -12,3 +12,14 @@ Handlebars.registerHelper('if_session', function(name, options) {
 		return options.fn(Session.get(name));
 	return "";
 });
+Handlebars.registerHelper("itemCount", function(auction, filter) {
+	if(!auction || !auction.items) {
+		return 0;
+	}
+	return _.reduce(auction.items, function(memo, item) {
+		if(filter && item.item.quality !== filter) {
+			return memo;
+		}
+		return memo + item.quantity;
+	}, 0);
+});
